0b4b9ba3096f7c889727082c7543af63022519bf
lebit <spam@labitte.com>
2023-01-20 16:38:05 +0100

    Update .config/nvim/lua/testing/init.lua

--- dot_config/nvim/lua/testing/init.lua
+++ dot_config/nvim/lua/testing/init.lua
@@ -1,16 +1,15 @@
 require("neotest").setup({
-    quickfix = {
-      enabled = true,
-      open = false
-    },
-        adapters = {
-                require('neotest-jest')({
-                        jestCommand = "npm test --",
-                        --jestConfigFile = "custom.jest.config.ts",
-                        env = { CI = true },
-                        cwd = function(path)
-                                return vim.fn.getcwd()
-                        end,
-                }),
-        }
+   quickfix = {
+     enabled = true,
+     open = true
+   },
+  adapters = {
+    require('neotest-jest')({
+      jestCommand = "npm test --",
+      env = { CI = true },
+      cwd = function(path)
+        return vim.fn.getcwd()
+      end,
+    }),
+  }
 })
