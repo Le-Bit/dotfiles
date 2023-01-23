9e9278f060eb5243f8442a35f0d6dfb793169163
lebit <spam@labitte.com>
2023-01-23 17:18:44 +0100

    Add .config/nvim/lua/lsp/empty_snippets.lua Update .config/nvim/lua/plugins/nvimtree.lua

--- dot_config/nvim/lua/plugins/nvimtree.lua
+++ dot_config/nvim/lua/plugins/nvimtree.lua
@@ -8,7 +8,29 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		show_on_open_dirs = true,
+		debounce_delay = 50,
+		severity = {
+			min = vim.diagnostic.severity.HINT,
+			max = vim.diagnostic.severity.ERROR,
+		},
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_root = false,
+		ignore_list = {},
+	},
+})
 
 local nvim_tree_events = require("nvim-tree.events")
 local bufferline_api = require("bufferline.api")
